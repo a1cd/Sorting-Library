c7fe184723bb7e6d9074baa33d5ae25ab4e2540a
a1cd <71281043+a1cd@users.noreply.github.com>
2021-07-16 21:17:19 -0400

    Added book and started page

---
+++ src/book.ts
@@ -0,0 +1,10 @@
+class Book {
+    Name: String
+    Author: String
+    Contents: String
+    constructor(Name, Author, contents) {
+        this.Name = Name
+        this.Author = Author
+        this.Contents = contents
+    }
+}
